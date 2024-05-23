bbd13a6892dbbd278227dba58c6ff444fe6ba369
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2024-05-23 22:29:33 +0400

    11-Mechul adında İnterface yaradın və iki metod təyin edin daha sonra Sinif adında class yaradıb miras alın və metodları override edin

---
+++ Java Exam Baau/src/sual_11/Mechul.java
@@ -0,0 +1,11 @@
+package sual_11;
+
+/**
+ * Mechul adında İnterface yaradın və iki metod təyin edin daha sonra Sinif adında class yaradıb miras alın və metodları override edin
+ *
+ */
+public interface Mechul {
+    void oxu(String kitab);
+    void yaz(String tapsiriq);
+
+}
---
+++ Java Exam Baau/src/sual_11/Sinif.java
@@ -0,0 +1,14 @@
+package sual_11;
+
+public class Sinif implements Mechul{
+    @Override
+    public void oxu(String kitab) {
+        System.out.println(kitab+"ı oxu");
+    }
+
+    @Override
+    public void yaz(String tapsiriq) {
+        System.out.println(tapsiriq+"ı yaz");
+
+    }
+}
