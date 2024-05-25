24afae4a9b209ad737a5b375496423015bc39c95
Ramazan Abdurahmanov <r0ma.abdurahmanov93@gmail.com>
2024-05-25 22:32:58 +0400

    34-İki ədədin cəmini və fərqini geri qaytaran iki parametrli metod yazın

---
+++ Java Exam Baau/src/sual_34/Main.java
@@ -0,0 +1,20 @@
+package sual_34;
+
+/**
+ * İki ədədin cəmini və fərqini geri qaytaran iki parametrli metod yazın.
+ */
+public class Main {
+    public static double cem(double a, double b) {
+        return a + b;
+    }
+
+    public static double ferq(double a, double b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(cem(2, 3));
+        System.out.println(ferq(5, 9));
+
+    }
+}
